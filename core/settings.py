2ff87111d4e6611c0bde411de72811b53d414588
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2025-09-08 21:23:45 +0200

    settings fixed

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+# --- Staticfiles ---
+STATIC_URL = "/backend-static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# (optional, falls du Uploads nutzt)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
